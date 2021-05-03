a6b023ed2dd4e25518954abf78d1697b80967d48
JDR <jdriverab@hotmail.com>
2021-05-03 19:31:26 -0400

    Se unifica calculadora con index y se crea a partir de DOM, session storage funcional y codigo actualizado

--- js/calculadoraJs.js
+++ js/calculadoraJs.js
@@ -1,15 +1,17 @@
-class InformacionPrestamo {
-    constructor(valoresIngresados){
-        this.monto = 0 + Number(valoresIngresados[0].value);
-        this.interes = 0 + Number(valoresIngresados[1].value);
-        this.meses = 0 + Number(valoresIngresados[2].value);
-        this.montoAPagarUnMes = 0;
-        this.montoAPagarTotal = 0;
-    
-    }
+let infoHistorica = 0
 
+class CalculadoraPrestamos{
+    constructor(valoresIngresados){
+      this.monto = Number(valoresIngresados[0].value);
+      this.interes = Number(valoresIngresados[1].value);
+      this.meses = Number(valoresIngresados[2].value);
+      this.montoAPagarUnMes = 0;
+      this.montoAPagarTotal = 0;
+      this.camposDeValores = valoresIngresados;
+    }
+  
     /**
-     * Este metodo recibe datos ingresados y regresa el valor a pagar por mes y por el total
+     * Metodo recibe datos ingresados y regresa el valor a pagar del prestamo por mes y por el total
      * @param {number} this.monto
      * @param {number} this.interes
      * @param {number} this.meses
@@ -17,68 +19,128 @@ class InformacionPrestamo {
      * @return {number} this.montoAPagarTotal
      */
     calculaResultadoPrestamo(){
+      this.montoAPagarTotal = Math.ceil((this.monto * (this.interes/1200) * this.meses) + this.monto)
+      this.montoAPagarUnMes = Math.ceil(this.montoAPagarTotal/this.meses)
+    }
+  
+    /**
+     * Metodo interactua con DOM para mostrar resultado y agregar boton de contacto, (recibe los datos calculados del prestamo, imprime en html los resultados y agrega un nuevo boton al layout de la calculadora)
+     * @param {number} this.monto
+     * @param {number} this.interes
+     * @param {number} this.meses
+     */
+    imprimeResultado(){
+  
+      document.getElementById("mensajeDatos").innerHTML = ``
+  
+      document.getElementById("divBotonCalculadora").innerHTML = `<div class="divBotonCalculadora">
+      <button type="button" class="btn btn-primary btn-block" onclick="capturaYEjecutaCalculadora()" id="botonCalculadora">Re-calcular</button>
+      <button type="button" class="btn btn-info btn-block" onclick="location.href='#contact-section'"style="margin-top:0px">Pide tu credito ya!</button>
+      </div>`
+  
+      document.getElementById("pagoMensual").innerHTML = `${this.montoAPagarUnMes.toLocaleString()} CLP`
+      document.getElementById("pagoTotal").innerHTML = `${this.montoAPagarTotal.toLocaleString()} CLP`
+    }
+  
+    /**
+     * Metodo almacena los datos del objeto creado en el local y sesion storage
+     * 
+     */
+     guardadoSessionStorage(){
+      let informacionUltimoPrestamo = [this.montoAPagarUnMes, this.montoAPagarTotal]
+      sessionStorage.setItem("ultimoPrestamo",JSON.stringify(informacionUltimoPrestamo))
+    }
+  
+  
+    /**
+     * Este metodo modifica el DOM; metodo principal con secuencia de ejecucion para calculadora (valida campos ingresados con numero, si cumple borra los valores del calculo anterior y ejecuta un nuevo calculo.
+     * @param {number} this.monto
+     * @param {number} this.interes
+     * @param {number} this.meses
+     */
+    validaCamposYEjecuta(){
+        document.getElementById("pagoMensual").innerHTML = ``
+        document.getElementById("pagoTotal").innerHTML = ``
+        var validador = true
+        for(let i = 0; i < this.camposDeValores.length; i++){
+            if(this.camposDeValores[i].value == ""){
+                document.getElementById("mensajeDatos").innerHTML = 'Debe ingresar datos en todos los campos'
+                this.camposDeValores[i].focus()
+                validador = false
+                break
+            }if(this.camposDeValores[i].value == 0){
+                document.getElementById("mensajeDatos").innerHTML = `El valor ingresado debe ser mayor a 0`
+                this.camposDeValores[i].focus()
+                validador = false
+                break
+            }
+        }
 
-        this.montoAPagarTotal = Math.ceil((this.monto * (this.interes/1200) * this.meses) + this.monto)
-        this.montoAPagarUnMes = Math.ceil(this.montoAPagarTotal/this.meses)
+        if(validador){
+            this.calculaResultadoPrestamo()
+            this.imprimeResultado()
+        }
     }
 }
-
-/**
-* captura los valores del formulario, los ingresa en array para crear el objeto e imprime los montos calculados del prestamo
+  
+/** 
+* ¡¡¡FUNCION INICIAL DE CALCULADORA!!! ¡¡Contiene secuencia de ejecucion de toda la calculadora (a excepcion de actualizaciones DOM)!! -> captura los valores del formulario con evento onClick, los ingresa en array y crea el objeto; luego ejecuta los el metodo principal del objeto y sube los resultados al storage
 *@param {number} monto
 *@param {number} interes
 *@param {number} meses
+*@event 
 */
-function capturaDatosYEjecutaCalculaPrestamo(){
+function capturaYEjecutaCalculadora(){
 
-    document.getElementById("pagoMensual").innerHTML = ``
-    document.getElementById("pagoTotal").innerHTML = ``
+  document.getElementById("pagoMensual").innerHTML = ``
+  document.getElementById("pagoTotal").innerHTML = ``
 
-    datosCapturados = [document.getElementById("monto"),document.getElementById("tasa"),document.getElementById("meses")]
-    validador = true
+  let informacionPrestamo = new CalculadoraPrestamos ([document.getElementById("monto"),document.getElementById("tasa"),document.getElementById("meses")])
 
-    for(i = 0; i < datosCapturados.length; i++){
-        if(datosCapturados[i].value == ""){
-            document.getElementById("mensajeDatos").innerHTML = 'Debe ingresar datos en todos los campos'
-            datosCapturados[i].focus()
-            validador = false
-            break
-        }if(datosCapturados[i].value == 0){
-            document.getElementById("mensajeDatos").innerHTML = `El valor ingresado debe ser mayor a 0`
-            datosCapturados[i].focus()
-            validador = false
-            break
-        }
+  informacionPrestamo.validaCamposYEjecuta()
+
+  if(informacionPrestamo.montoAPagarTotal > 0){
+
+    infoStorage = JSON.parse(localStorage.getItem("historialPrestamo"))
+
+    if (infoStorage == null){
+        infoHistorica = [informacionPrestamo]
+    } else {
+        infoHistorica = infoStorage
+        infoHistorica.push(informacionPrestamo)
     }
+    localStorage.setItem("historialPrestamo",JSON.stringify(infoHistorica))
+  }
 
-    if(validador){
+  ultimoCalculo = JSON.parse(sessionStorage.getItem("ultimoPrestamo"))
 
-        let informacionPrestamo = new InformacionPrestamo(datosCapturados)
+  console.log(ultimoCalculo)
+
+  if(ultimoCalculo != null){
     
-        informacionPrestamo.calculaResultadoPrestamo()
+    document.getElementById("mensajeUltimoCalculo").innerHTML = `<br>-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°<br><h4>Total a pagar calculo anterior: 
+    ${ultimoCalculo[1]}<br><small>Valor mensual calculo anterior: ${ultimoCalculo[0]}</small></h4>`
+    
+  }else{
+    document.getElementById("mensajeUltimoCalculo").innerHTML = ``
+  }
 
-        document.getElementById("mensajeDatos").innerHTML = ` `
-
-        document.getElementById("divBotonCalculadora").innerHTML = `<div class="divBotonCalculadora">
-        <button type="button" class="btn btn-primary btn-block" onclick="capturaDatosYEjecutaCalculaPrestamo()" id="botonCalculadora">Re-calcular</button>
-        <button type="button" class="btn btn-info btn-block" onclick="location.href='#contact-section'"style="margin-top:0px">Contactanos!</button>
-        </div>`
-
-        document.getElementById("pagoMensual").innerHTML = `${informacionPrestamo.montoAPagarUnMes.toLocaleString()} CLP`
-        document.getElementById("pagoTotal").innerHTML = `${informacionPrestamo.montoAPagarTotal.toLocaleString()} CLP`
-
-        localStorage.setItem("prestamo",JSON.stringify(informacionPrestamo))
-    }
+  informacionPrestamo.guardadoSessionStorage()
 }
-
+  
 /**
-* modifica el indicador de meses en html segun el evento onmovemouse
-*@param {Event}
+ * Funcion modifica el DOM; actualiza indicador de meses en html segun el evento onmovemouse
+ *@event 
 */
 function actualizaMeses(){
     document.getElementById("rangoMeses").innerHTML = `${document.getElementById("meses").value}`
 }
 
+
+/**
+* Funcion modifica el layout del index para que se muestre la calculadora al usar boton
+*@event 
+*/
 function crearCalculadora() {
     document.getElementById("espacioCalculadora").innerHTML = `<div class="container">
     <div>
@@ -106,14 +168,14 @@ function crearCalculadora() {
 
           </div>
           <div id="divBotonCalculadora">
-              <button type="button" class="btn btn-primary btn-block" onclick="capturaDatosYEjecutaCalculaPrestamo()"> Calcular</button>
+              <button type="button" class="btn btn-primary btn-block" onclick="capturaYEjecutaCalculadora()"> Calcular</button>
 
           </div>
 
         </form>
-        <h2 id="mensajeDatos"></h2><h3>Total a pagar: <span id="pagoTotal"></span><br>
-        <small>Valor cuota mensual: <span id="pagoMensual"></span></small></h3>
-
+        <div><h2 id="mensajeDatos"></h2><h3>Total a pagar: <span id="pagoTotal"></span><br>
+        <small>Valor cuota mensual: <span id="pagoMensual"></span></small></h3></div>
+        <div id="mensajeUltimoCalculo"></div>
 
       </div>
     </div>
